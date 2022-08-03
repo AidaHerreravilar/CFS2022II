1befc963b10fc881b814dbd07b8b04c253c1baf1
AidaHerreravilar <aidaherreravilar383@gmail.com>
2022-08-03 17:23:41 -0300

    ejercicios resueltos

--- CLASE6this/Gestorlibros.ts
+++ CLASE6this/Gestorlibros.ts
@@ -1,57 +1,35 @@
 import * as ReadlineSync from  "readline-sync";
-import {libro} from "./basededatoslibros"
-class gestorLibros{
-    libros: libro []= [];
+import {Libro} from "./basededatoslibros"
+
+class gestorLibro {
+    libro: Libro []= [];
+
     constructor(){
-    this.libros=[new libro ("Cien Años de Soledad", 916), new libro ("El Coronel No Tiene Quien Le Escriba", 102)];
+    this.libro=[new Libro ("Cien Años de Soledad", 916), new Libro ("El Coronel No Tiene Quien Le Escriba", 102)];
 
 }
 insertarLibro(name:string,pagina:number){
-    this.libros.push(new libro(name,pagina));
+    this.libro.push(new Libro(name,pagina));
 
 }
-consultarLibro(){
-    let nombreLibro: string = ReadlineSync.quiestion("Nombre del Libro que Busca") {
-                this.libros.forEach((object: libro) => {
+consultarLibro() {
+    let nombreLibro: string = ReadlineSync.question("Nombre del Libro que Busca");
+                this.libro.forEach(object => {    
             if (object.getNombre () === nombreLibro) {
                 console.log("Ingrese Nombre del nuevo Libro");
             }
+});}
 
-}
     modificarLibro() {
-        let numero=ReadlineSync.question(numeroModificado);
-        let libro=ReadlineSync.question(libroModificado);
-        libreria[numero]=libro;
-        console.log(libreria);
-    }
-    }
-    }
-}
-function eliminar() {
-    throw new Error("Function not implemented.");
-}
-
-function modificar() {
-    throw new Error("Function not implemented.");
-}
-
-function libroEliminado(libroEliminado: any) {
-    throw new Error("Function not implemented.");
-}
-
-function libreria(libreria: any) {
-    throw new Error("Function not implemented.");
-}
-
-function libroModificado(libroModificado: any) {
-    throw new Error("Function not implemented.");
-}
-
-function eliminarLibro() {
-    throw new Error("Function not implemented.");
-}
-
-function modificarLibro() {
-    throw new Error("Function not implemented.");
-}
-
+    let preglibro: string = ReadlineSync.question("libro a modificar");
+    this.libro.forEach(object => {
+        if (object.getNombre () === preglibro) {
+            console.log("datos del libro nuevo: ");
+        }
+    });}
+     eliminarLibro () {  
+    let preglibro: string = ReadlineSync.question("libro a eliminar");
+    this.libro.forEach(object => {
+        if (object.getNombre () === preglibro) {
+            console.log("libro eliminado");
+        }})}}
